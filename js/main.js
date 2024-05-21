6e8154ebc3c429a50061bcb4662361bb42e2471e
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-05-21 02:32:50 -0300

    Swiper cards versão mobile e desktop prontas!

--- js/main.js
+++ js/main.js
@@ -12,3 +12,34 @@ mobileButton.addEventListener('click', menuShow = (e) => {
         menuMobile.classList.add('open');
     }
 })
+
+let swiper = new Swiper('.slide-content', {
+    loop: true,
+    slidesPerView: 3,
+    grabCursor: true,
+    spaceBetween: 22,
+  
+    pagination: {
+      el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        520: {
+          slidesPerView: 2,
+        },
+        780: {
+            slidesPerView: 3,
+        }
+    },
+
+});
