c61f0c38bad65da459902e914a2987a8a7d156e3
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-05-25 12:42:18 -0300

    Efeito ScrollReveal implementado

---
+++ js/reveal.js
@@ -0,0 +1,45 @@
+
+window.revelar = ScrollReveal({reset: true})
+
+// HERO SECTION
+revelar.reveal('.efeito-hero-section', {
+    duration: 1500,
+    distance: '50px',
+    origin: 'left'
+})
+
+// SOBRE NOS SECTION
+revelar.reveal('.efeito-sobre-nos-section', {
+    duration: 1600,
+    distance: '80px'
+})
+
+// CARDS SECTION
+revelar.reveal('.efeito-cards-section', {
+    duration: 1600,
+    distance: '80px'
+})
+
+// AVALIAÇÕES SECTION
+revelar.reveal('.efeito-avaliacoes-section', {
+    duration: 1600,
+    distance: '80px'
+})
+
+revelar.reveal('.efeito-avaliacao-card1', {
+    duration: 1600,
+    distance: '80px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-avaliacao-card2', {
+    duration: 1600,
+    distance: '80px',
+    delay: 800
+})
+
+// ENDEREÇO SECTION
+revelar.reveal('.efeito-endereco-section', {
+    duration: 1600,
+    distance: '80px'
+})
